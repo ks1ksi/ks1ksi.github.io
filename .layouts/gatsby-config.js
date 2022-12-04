c5f0e30c0c3689dc50d098d22ccbd7ca0f3fae33
Seungil Kim <ksi990302@gmail.com>
2022-12-04 18:35:28 +0900

    change config

--- .layouts/gatsby-config.js
+++ .layouts/gatsby-config.js
@@ -17,22 +17,12 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        plugins: [
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-highlight-code`,
-            options: {
-              terminal: "carbon",
-              theme: "blackboard",
-            },
-          },
-          {
-            resolve: `gatsby-remark-katex`,
-            options: {
-              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`,
-            },
           },
         ],
       },
