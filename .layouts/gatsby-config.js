8230ede794d6b60dc4d52b92b4128af0d64189f4
Seungil Kim <ksi990302@gmail.com>
2022-12-04 17:41:45 +0900

    add katex and highlight-code

--- .layouts/gatsby-config.js
+++ .layouts/gatsby-config.js
@@ -17,6 +17,27 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-highlight-code`,
+            options: {
+              terminal: "carbon",
+              theme: "blackboard",
+            },
+          },
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`,
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "content",
